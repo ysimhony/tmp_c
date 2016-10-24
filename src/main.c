2954f9b3b0c7eae77d57e74b8f08f8e88506cffe
Yacov Simhony <ysimhony@gmail.com>
2016-10-25 00:11:07 +0300

    new file

---
+++ src/main.c
@@ -0,0 +1,33 @@
+#include "stdio.h"
+
+
+char *opcodes = 
+{  
+"mov"
+"cmp"
+"add"
+"sub"
+"not"
+"clr"
+"lea"
+"inc"
+"dec"
+"jmp"
+"bne"
+"red"
+"prn"
+"jsr"
+"rts"
+"stop"
+};
+
+
+
+int main() 
+{
+   
+   printf("hellow world!!!\n");
+   
+   
+
+};
